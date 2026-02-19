088f67ca80d3e86965205bfa64a380a213267966
SHANAL2525 <s22010357@ousl.lk>
2026-02-19 21:33:34 +0530

    some updates

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -3,7 +3,7 @@ export const projects = [
     title: "Factory Machine Monitoring System",
     description:
       "Team-based project focused on monitoring factory machine operations and system status. Built collaboratively using GitHub workflows.",
-    tech: ["Vue.js", "Team Project", "GitHub"],
+    tech: ["Vue.js", "Firebase" , "GitHub" , "Team Project"],
     github: "https://github.com/Group-Project-Ousl/factory-machine-monitoring-system.git",
     live: "",
   },
@@ -11,16 +11,16 @@ export const projects = [
     title: "Nuranova Solutions Website",
     description:
       "Responsive business website with clean layout and structured sections to present services and company information.",
-    tech: ["HTML", "CSS", "JavaScript"],
+    tech: ["React", "CSS", "JavaScript"],
     github: "https://github.com/kawshal43/nuranovasolutions.git",
     live: "",
   },
   {
     title: "Student Management System (Java Swing)",
     description:
-      "Desktop application to manage student records with CRUD features, validation, and a structured UI. (Repo coming soon.)",
-    tech: ["Java", "Swing"],
-    github: "",
+      "Desktop application to manage student records with CRUD features, validation, and a structured UI.",
+    tech: ["Java", "Swing","MySQL"],
+    github: "https://github.com/SHANAL2525/java_swing_shanal.git",
     live: "",
   },
 ] as const;
